b56330ff2fa9846aa9da548ee78cd16bf8d3a7b8
midhyun <kfree912@gmail.com>
2023-01-04 18:29:28 +0900

    migrate_group

--- attendances/models.py
+++ attendances/models.py
@@ -3,24 +3,25 @@ from django.conf import settings
 # Create your models here.
 
 class Attendances(models.Model):
-    users = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='user')
+    users = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='attendaces')
     check_in = models.DateTimeField(null=True, auto_now_add=True)
     check_out = models.DateTimeField(null=True, blank=True)
-    status = models.CharField(null=True)
+    status = models.CharField(null=True, max_length=30)
 
 class AttendancesCount(models.Model):
-    users = models.ForeignKey(settings.AUTH_USER_MDOEL, on_delete=models.CASCADE, related_name='user')
+    users = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='count')
     attendance_days = models.IntegerField(null=True)
     absent_days = models.IntegerField(null=True)
     tardy_days = models.IntegerField(null=True)
     leave_days = models.IntegerField(null=True)
 
-class BelongGroup(models.Model):
-    users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='user')
-    group = models.CharField
 
 class GroupInfo(models.Model):
-    host = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='host')
+    host = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='group')
     name = models.CharField(max_length=30)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+class BelongGroup(models.Model):
+    users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='belong')
+    group = models.ForeignKey(GroupInfo, on_delete=models.CASCADE, related_name='members')
