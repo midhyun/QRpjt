e26ad1fe4579e082a5a5f51bf4d089ed8dba4005
midhyun <kfree912@gmail.com>
2023-02-20 21:20:40 +0900

    setting_pjt

--- attendances/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'attendances'
-
-urlpatterns = [
-    path('', views.AttendancesList.as_view()),
-    path('<int:pk>/', views.AttendancesDetail.as_view()),
-]
--- shortener/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Url(models.Model):
-    link = models.URLField(max_length=200)
-    new_link = models.URLField(default="")
-    expire = models.DateTimeField()
